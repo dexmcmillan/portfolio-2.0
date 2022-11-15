923d35af4402b77b675e27da110bab76a27df41a
dexmcmillan <dexmcmillan@gmail.com>
2022-11-15 08:54:55 -0500

    production-ready

--- frontend/composables/getData.js
+++ frontend/composables/getData.js
@@ -1,55 +1,10 @@
-export const getProjects = async () => {
+// Comment out for dev or production database.
 
-    const config = useRuntimeConfig()
+// const baseUrl = "http://localhost:8000"
+const baseUrl = "https://data.dextermcmillan.com"
 
-    let baseUrl
+export const getProjects = async () => { return await useFetch(`${baseUrl}/api/projects`) }
 
-    // If we're in dev mode, use testing/dev database.
-    if (config.mode != 'production') {
-        baseUrl = "http://localhost:8000"
-    }
+export const getTags = async () => { return await useFetch(`${baseUrl}/api/tags`) }
 
-    // Otherwise, use the real one.
-    else {
-        baseUrl = "https://data.dextermcmillan.com"
-    }
-
-    return await useFetch(`${baseUrl}/api/projects`)
-}
-
-export const getTags = async () => {
-
-    const config = useRuntimeConfig()
-
-    let baseUrl
-
-    // If we're in dev mode, use testing/dev database.
-    if (config.mode != 'production') {
-        baseUrl = "http://localhost:8000"
-    }
-
-    // Otherwise, use the real one.
-    else {
-        baseUrl = "https://data.dextermcmillan.com"
-    }
-
-    return await useFetch(`${baseUrl}/api/tags`)
-}
-
-export const getJobs = async () => {
-
-    const config = useRuntimeConfig()
-
-    let baseUrl
-
-    // If we're in dev mode, use testing/dev database.
-    if (config.mode != 'production') {
-        baseUrl = "http://localhost:8000"
-    }
-
-    // Otherwise, use the real one.
-    else {
-        baseUrl = "https://data.dextermcmillan.com"
-    }
-    return await useFetch(`${baseUrl}/api/jobs`)
-}
+export const getJobs = async () => { return await useFetch(`${baseUrl}/api/jobs`) }
